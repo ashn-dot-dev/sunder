856422487b08fbed6b921097c803bccb263b926a
ashn <me@ashn.dev>
2021-09-20 22:38:56 -0400

    Add cat example
    This changeset adds a (not strictly POSIX conformant) version of the
    standard Unix utility `cat` to the examples directory.
    
    This changeset also adds a slew of new utility functions to the standard
    library, most of which were added to support the development of the
    `cat` program.

--- examples/build-examples.sh
+++ examples/build-examples.sh
@@ -7,5 +7,5 @@ export SUNDER_IMPORT_PATH="${SUNDER_HOME}/lib"
 for f in *.sunder; do
     CMD="${SUNDER_HOME}/bin/sunder-compile -o ${f%.sunder} ${f}"
     echo $CMD
-    exec $CMD
+    eval $CMD
 done
