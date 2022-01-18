e8ae398031c7ebe500d6e06c3e20ce076b9ab751
ashn <me@ashn.dev>
2022-01-18 08:42:40 -0500

    Remove -k flag from examples.build.sh
    This flag is not necessary for new users who are looking at example
    programs. The extra .o and .asm files are more of a distraction than a
    help. If a user desires to view the generated assembly or object file
    then they may always compile the example themselves with the -k flag.

--- examples/examples.build.sh
+++ examples/examples.build.sh
@@ -5,7 +5,7 @@ export SUNDER_HOME="$(realpath ..)"
 export SUNDER_IMPORT_PATH="${SUNDER_HOME}/lib"
 
 for f in *.sunder; do
-    CMD="${SUNDER_HOME}/bin/sunder-compile -k -o ${f%.sunder} ${f}"
+    CMD="${SUNDER_HOME}/bin/sunder-compile -o ${f%.sunder} ${f}"
     echo $CMD
     eval $CMD
 done
