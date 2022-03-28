a6cb310f56fb88dfb4c158300c700d7d1951cd3f
ashn <me@ashn.dev>
2022-03-28 08:47:21 -0400

    Update deps/autil/autil.h to v1.0.0

--- deps/autil/autil.h
+++ deps/autil/autil.h
@@ -1,5 +1,5 @@
 /*
-AUTIL - ASHN'S UTILITY LIBRARY - v0.11.0
+AUTIL - ASHN'S UTILITY LIBRARY - v1.0.0
     Single header file containing functions and data structures for rapid
     application development in C99 (or later) with a focus on simplicity.
     This library has no dependencies outside of libc.
