9c7c20bbd3274cff900c6fa2f831035a42253059
ashn <me@ashn.dev>
2022-06-30 19:28:59 -0400

    Add std::error as the standard error type

--- misc/errno-list.sh
+++ misc/errno-list.sh
@@ -4,8 +4,12 @@
 set -e
 
 # The errno program comes from moreutils.
+#
 # https://joeyh.name/code/moreutils/
-# apt install moreutils
+#
+# ```sh
+# $ apt install moreutils
+# ```
 which errno >/dev/null
 
 # The output of errno uses the form:
@@ -16,9 +20,29 @@ MAX=$(errno --list | cut -d ' ' -f 2 | sort -n | tail -n 1)
 # The value of length + 1 is used so that a constant 'FOO' can be rendered as
 # 'FOO:<spaces>ssize' instead of 'FOO<spaces>: ssize'.
 SYMBOL_WIDTH=$(errno --list | cut -d ' ' -f 1 | awk '{ print length + 1 }' | sort -n | tail -n 1)
-# The value of length + 3 is used so that there is room for the opening and
-# closing double quotes around the string plus an optional comma.
-STRING_WIDTH=$(errno --list | cut -d ' ' -f 3- | awk '{ print length + 3 }' | sort -n | tail -n 1)
+
+value() {
+    SYMBOL=$1
+    STRING=$2
+    printf "\"[system error ${SYMBOL}] ${STRING}\""
+}
+
+STRING_WIDTH=0
+for i in $(seq 0 "${MAX}"); do
+    if ! errno "${i}" >/dev/null 2>&1; then
+        continue
+    fi
+
+    OUTPUT=$(errno "${i}")
+    SYMBOL=$(echo "${OUTPUT}" | cut -d ' ' -f 1)
+    NUMBER=$(echo "${OUTPUT}" | cut -d ' ' -f 2)
+    STRING=$(echo "${OUTPUT}" | cut -d ' ' -f 3-)
+
+    WIDTH=$(value "${SYMBOL}" "${STRING}" | wc -c)
+    if [ "${WIDTH}" -gt "${STRING_WIDTH}" ]; then
+        STRING_WIDTH="${WIDTH}"
+    fi
+done
 
 for i in $(seq "${MIN}" "${MAX}"); do
     if ! errno "${i}" >/dev/null 2>&1; then
@@ -33,26 +57,25 @@ done
 
 printf '\n'
 
-echo "const ERRNO_STRINGS: [][]byte = (:[][]byte)["
+echo "const ERRNO_STRS = (:[][]byte)["
 for i in $(seq 0 "${MAX}"); do
     COMMA=','
     if [ "${i}" = "${MAX}" ]; then
         COMMA=''
     fi
 
-    if ! errno "${i}" >/dev/null 2>&1; then
-        SYMBOL="Undefined errno value"
+    if errno "${i}" >/dev/null 2>&1; then
+        OUTPUT=$(errno "${i}")
+        SYMBOL=$(echo "${OUTPUT}" | cut -d ' ' -f 1)
+        NUMBER=$(echo "${OUTPUT}" | cut -d ' ' -f 2)
+        STRING=$(echo "${OUTPUT}" | cut -d ' ' -f 3-)
+    else
+        SYMBOL="${i}"
         NUMBER="${i}"
-        STRING="${SYMBOL}"
-        printf "    %-${STRING_WIDTH}s # ${SYMBOL} (${NUMBER})\n" "\"${STRING}\"${COMMA}"
-        continue
+        STRING="Undefined errno value"
     fi
 
-    OUTPUT=$(errno "${i}")
-    SYMBOL=$(echo "${OUTPUT}" | cut -d ' ' -f 1)
-    NUMBER=$(echo "${OUTPUT}" | cut -d ' ' -f 2)
-    STRING=$(echo "${OUTPUT}" | cut -d ' ' -f 3-)
-    printf "    %-${STRING_WIDTH}s # ${SYMBOL} (${NUMBER})\n" "\"${STRING}\"${COMMA}"
-
+    VALUE="$(value "${SYMBOL}" "${STRING}")${COMMA}"
+    printf "    %-${STRING_WIDTH}s%s\n" "${VALUE}"
 done
 echo "];"
