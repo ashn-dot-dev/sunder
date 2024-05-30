0421beeacfaca38316eb673a5e5872e5d04d01ae
ashn <me@ashn.dev>
2024-05-30 08:19:14 -0400

    Remove references to custom system assembly in examples.clean.sh

--- examples/examples.clean.sh
+++ examples/examples.clean.sh
@@ -3,18 +3,11 @@ set -e
 
 for f in *.sunder; do
     rm -f "${f%.sunder}"
-    rm -f "${f%.sunder}.asm"
     rm -f "${f%.sunder}.o"
     rm -f "${f%.sunder}.html"
 done
 
 rm -f \
-    custom-sys-asm/custom-sys-asm \
-    custom-sys-asm/custom-sys-asm.asm \
-    custom-sys-asm/custom-sys-asm.o
-
-rm -f \
     ffi/calling-c-from-sunder \
     ffi/calling-sunder-from-c \
-    ffi/*.asm \
     ffi/*.o
