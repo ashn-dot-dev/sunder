d9be1c26969df6d65248c86374a93bdf7a58bc9a
ashn <me@ashn.dev>
2026-08-03 23:48:12 -0400

    Run clang-format

--- codegen.c
+++ codegen.c
@@ -1751,7 +1751,8 @@ codegen_stmt_continue(struct stmt const* stmt)
     assert(stmt != NULL);
     assert(stmt->kind == STMT_CONTINUE);
 
-    codegen_defers(stmt->data.continue_.defer_begin, stmt->data.continue_.defer_end);
+    codegen_defers(
+        stmt->data.continue_.defer_begin, stmt->data.continue_.defer_end);
     appendli("continue;");
 }
 
